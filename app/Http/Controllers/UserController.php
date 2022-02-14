50f220b9a815bb5e07a9bb0fbce27dfe63cab4c1
aleksandrboichuk <90320430+aleksandrboichuk@users.noreply.github.com>
2022-02-14 14:15:53 +0200

    admin-panel (products)

---
+++ app/Http/Controllers/UserController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use App\Models\OrdersList;
+use App\Models\StatusList;
+use Illuminate\Http\Request;
+
+class UserController extends Controller
+{
+    public function getUserOrders(){
+        $user_orders = OrdersList::where('user_id', $this->getUser()->id)->get();
+
+        return view('personal-area.orders',[
+            'user' => $this->getUser(),
+           'orders' => $user_orders,
+           'statuses' => StatusList::all(),
+        ]);
+    }
+
+    public function viewUserOrder($order_id){
+        $order = OrdersList::find($order_id);
+        $statuses = StatusList::all();
+
+        foreach ($statuses as $s){
+            if($order->status == $s->id ){
+                $status = $s->name;
+            }
+        }
+        return view('personal-area.view-order',[
+            'user' => $this->getUser(),
+            'status' => isset($status) ? $status : "-",
+            'order' => $order,
+            'items' =>  $order->items
+        ]);
+    }
+}
