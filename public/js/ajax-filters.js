1b1a5fd1247e9b5cae1f1edbc22d95fc5c4c0438
aleksandrboichuk <90320430+aleksandrboichuk@users.noreply.github.com>
2022-03-06 23:11:25 +0200

    view for elastic

--- public/js/ajax-filters.js
+++ public/js/ajax-filters.js
@@ -24,7 +24,7 @@ function indexAjax(url) {
         // });
         // $('.size').find('input[type="checkbox"]').change(function () {
         //     $('.size').find('input[type="checkbox"]').not(this).prop('checked', false);
-        });
+        //});
 
 
         $('.btn-danger-filters').click(function () {
@@ -124,117 +124,117 @@ function indexAjax(url) {
         });
 
         $(document).mouseup( function(e){
-            var div = $( ".filter-item" );
-//ajax
-            // if( div.find('.fil-params').hasClass('fil-active')){
-            //     let orderBy = $('select[name="order-by"]').val();
-            //     let from_price = parseInt($('input[name="from-price"]').val());
-            //     let to_price = parseInt($('input[name="to-price"]').val());
-            //
-            //     var isColor = false, isBrand = false, isMaterial = false, isSeason = false, isSize = false;
-            //     /* colors array */
-            //     for (let i = 0; i < color.length; i++) {
-            //         if (color[i].firstChild.checked) {
-            //             colors = color[i].textContent;
-            //             isColor = true;
-            //             if(colors != "Всі") {
-            //                 document.getElementById('color-title').textContent = "Колір (1)";
-            //             }else{
-            //                 document.getElementById('color-title').textContent = "Колір";                        }
-            //         }
-            //         if( i+1 == color.length && !isColor){
-            //             color[0].firstChild.checked = true;
-            //         }
-            //     }
-            //
-            //     /* brands array */
-            //
-            //     for (let i = 0; i < brand.length; i++) {
-            //         if (brand[i].firstChild.checked) {
-            //             brands = brand[i].textContent;
-            //             isBrand = true;
-            //             if(brands != "Всі") {
-            //                 document.getElementById('brand-title').textContent = "Бренд (1)";
-            //             }else{
-            //                 document.getElementById('brand-title').textContent = "Бренд";
-            //             }
-            //         }
-            //         if( i+1 == brand.length && !isBrand){
-            //             brand[0].firstChild.checked = true;
-            //         }
-            //     }
-            //
-            //     /* materials array */
-            //     for (let i = 0; i < material.length; i++) {
-            //         if (material[i].firstChild.checked) {
-            //             materials = material[i].textContent;
-            //             isMaterial = true;
-            //             if(materials != "Всі") {
-            //                 document.getElementById('material-title').textContent = "Матеріал (1)";
-            //             }else{
-            //                 document.getElementById('material-title').textContent = "Матеріал";
-            //             }
-            //         }
-            //         if( i+1 == material.length && !isMaterial){
-            //             material[0].firstChild.checked = true;
-            //         }
-            //     }
-            //
-            //     /* sizes array */
-            //     for (let i = 0; i < size.length; i++) {
-            //         if (size[i].firstChild.checked) {
-            //             sizes = size[i].textContent;
-            //             isSize = true;
-            //             if(sizes != "Всі") {
-            //                 document.getElementById('size-title').textContent = "Розмір (1)";
-            //             }else{
-            //                 document.getElementById('size-title').textContent = "Розмір";
-            //             }
-            //         }
-            //         if( i+1 == size.length && !isSize){
-            //             size[0].firstChild.checked = true;
-            //         }
-            //     }
-            //     /* seasons array */
-            //     for (let i = 0; i < season.length; i++) {
-            //         if (season[i].firstChild.checked) {
-            //             seasons = season[i].textContent;
-            //             isSeason = true;
-            //             if(seasons != "Всі"){
-            //                 document.getElementById('season-title').textContent = "Сезон (1)";
-            //             }else{
-            //                 document.getElementById('season-title').textContent = "Сезон";
-            //             }
-            //         }
-            //         if( i+1 == season.length && !isSeason){
-            //             season[0].firstChild.checked = true;
-            //         }
-            //     }
-            //
-            //     if ((colors != "") || (brands != "") || (materials != "")  || (seasons != "")  || (sizes != "") || !isNaN(from_price) || !isNaN(to_price)){
-            //         $.ajax({
-            //             url: url ,
-            //             type: "GET",
-            //             data: {
-            //                 colors: colors,
-            //                 brands: brands,
-            //                 materials: materials,
-            //                 seasons: seasons,
-            //                 sizes: sizes,
-            //                 orderBy: orderBy,
-            //                 from_price: !isNaN(from_price) ? from_price : 0,
-            //                 to_price: !isNaN(to_price) || to_price == 0 ? to_price : 1000000
-            //                 // countries: countries
-            //             },
-            //             headers: {
-            //                 'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
-            //             },
-            //             success: function(data){
-            //                 $('.products').html(data)
-            //             }
-            //         });
-            //     }
-            // }
+//             var div = $( ".filter-item" );
+// //ajax
+//             if( div.find('.fil-params').hasClass('fil-active')){
+//                 let orderBy = $('select[name="order-by"]').val();
+//                 let from_price = parseInt($('input[name="from-price"]').val());
+//                 let to_price = parseInt($('input[name="to-price"]').val());
+//
+//                 var isColor = false, isBrand = false, isMaterial = false, isSeason = false, isSize = false;
+//                 /* colors array */
+//                 for (let i = 0; i < color.length; i++) {
+//                     if (color[i].firstChild.checked) {
+//                         colors = color[i].textContent;
+//                         isColor = true;
+//                         if(colors != "Всі") {
+//                             document.getElementById('color-title').textContent = "Колір (1)";
+//                         }else{
+//                             document.getElementById('color-title').textContent = "Колір";                        }
+//                     }
+//                     if( i+1 == color.length && !isColor){
+//                         color[0].firstChild.checked = true;
+//                     }
+//                 }
+//
+//                 /* brands array */
+//
+//                 for (let i = 0; i < brand.length; i++) {
+//                     if (brand[i].firstChild.checked) {
+//                         brands = brand[i].textContent;
+//                         isBrand = true;
+//                         if(brands != "Всі") {
+//                             document.getElementById('brand-title').textContent = "Бренд (1)";
+//                         }else{
+//                             document.getElementById('brand-title').textContent = "Бренд";
+//                         }
+//                     }
+//                     if( i+1 == brand.length && !isBrand){
+//                         brand[0].firstChild.checked = true;
+//                     }
+//                 }
+//
+//                 /* materials array */
+//                 for (let i = 0; i < material.length; i++) {
+//                     if (material[i].firstChild.checked) {
+//                         materials = material[i].textContent;
+//                         isMaterial = true;
+//                         if(materials != "Всі") {
+//                             document.getElementById('material-title').textContent = "Матеріал (1)";
+//                         }else{
+//                             document.getElementById('material-title').textContent = "Матеріал";
+//                         }
+//                     }
+//                     if( i+1 == material.length && !isMaterial){
+//                         material[0].firstChild.checked = true;
+//                     }
+//                 }
+//
+//                 /* sizes array */
+//                 for (let i = 0; i < size.length; i++) {
+//                     if (size[i].firstChild.checked) {
+//                         sizes = size[i].textContent;
+//                         isSize = true;
+//                         if(sizes != "Всі") {
+//                             document.getElementById('size-title').textContent = "Розмір (1)";
+//                         }else{
+//                             document.getElementById('size-title').textContent = "Розмір";
+//                         }
+//                     }
+//                     if( i+1 == size.length && !isSize){
+//                         size[0].firstChild.checked = true;
+//                     }
+//                 }
+//                 /* seasons array */
+//                 for (let i = 0; i < season.length; i++) {
+//                     if (season[i].firstChild.checked) {
+//                         seasons = season[i].textContent;
+//                         isSeason = true;
+//                         if(seasons != "Всі"){
+//                             document.getElementById('season-title').textContent = "Сезон (1)";
+//                         }else{
+//                             document.getElementById('season-title').textContent = "Сезон";
+//                         }
+//                     }
+//                     if( i+1 == season.length && !isSeason){
+//                         season[0].firstChild.checked = true;
+//                     }
+//                 }
+//
+//                 if ((colors != "") || (brands != "") || (materials != "")  || (seasons != "")  || (sizes != "") || !isNaN(from_price) || !isNaN(to_price)){
+//                     $.ajax({
+//                         url: url ,
+//                         type: "GET",
+//                         data: {
+//                             colors: colors,
+//                             brands: brands,
+//                             materials: materials,
+//                             seasons: seasons,
+//                             sizes: sizes,
+//                             orderBy: orderBy,
+//                             from_price: !isNaN(from_price) ? from_price : 0,
+//                             to_price: !isNaN(to_price) || to_price == 0 ? to_price : 1000000
+//                             // countries: countries
+//                         },
+//                         headers: {
+//                             'X-CSRF-TOKEN': $('meta[name="csrf-token"]').attr('content')
+//                         },
+//                         success: function(data){
+//                             $('.products').html(data)
+//                         }
+//                     });
+//                 }
+//             }
 
             if ( !div.is(e.target)
                 && div.has(e.target).length === 0 ) {
@@ -243,5 +243,5 @@ function indexAjax(url) {
             }
 
         })
-    // })
+     })
 }
--- resources/views/parts/filters.blade.php
+++ resources/views/parts/filters.blade.php
@@ -24,7 +24,6 @@
                         </div>
                         <div class="fil-params">
                             <ul>
-                                <li class="filter-check brand" data-filter="no"><input class="filter-input" autocomplete="off" id="no-filter-brand" type="checkbox" name="Brand" checked><label for="no-filter-brand">Всі</label></li>
                                 @foreach($brands as $b)
                                     <li class="filter-check brand" data-filter="{{$b->seo_name}}-brand"><input class="filter-input" autocomplete="off" id="{{$b->seo_name}}" type="checkbox" name="Brand"><label for="{{$b->seo_name}}">{{$b->name}}</label> </li>
                                 @endforeach
@@ -42,7 +41,6 @@
                         </div>
                         <div class="fil-params">
                             <ul>
-                                <li class="filter-check color" data-filter="no"><input class="filter-input" autocomplete="off" id="no-filter-color" type="checkbox" name="Color" checked><label for="no-filter-color">Всі</label></li>
                                 @foreach($colors as $c)
                                     <li class="filter-check color" data-filter="{{$c->seo_name}}-color"><input class="filter-input" autocomplete="off" id="{{$c->seo_name}}" type="checkbox" name="Color"><label for="{{$c->seo_name}}">{{$c->name}}</label> </li>
                                 @endforeach
@@ -60,7 +58,6 @@
                         </div>
                         <div class="fil-params">
                             <ul>
-                                <li class="filter-check material" data-filter="no"><input class="filter-input" autocomplete="off" id="no-filter-material" type="checkbox" name="Material" checked><label for="no-filter-material">Всі</label></li>
                                 @foreach($materials as $m)
                                     <li class="filter-check material" data-filter="{{$m->seo_name}}-material"><input class="filter-input" autocomplete="off" id="{{$m->seo_name}}" type="checkbox" name="Material"><label for="{{$m->seo_name}}">{{$m->name}}</label> </li>
                                 @endforeach
@@ -77,7 +74,6 @@
                         </div>
                         <div class="fil-params">
                             <ul>
-                                <li class="filter-check season" data-filter="no"><input class="filter-input" autocomplete="off" id="no-filter-season" type="checkbox" name="Season" checked><label for="no-filter-season">Всі</label></li>
                                 @foreach($seasons as $s)
                                     <li class="filter-check season" data-filter="{{$s->seo_name}}-season"><input class="filter-input" autocomplete="off" id="{{$s->seo_name}}" type="checkbox" name="Season"><label for="{{$s->seo_name}}">{{$s->name}}</label></li>
                                 @endforeach
@@ -94,7 +90,6 @@
                         </div>
                         <div class="fil-params">
                             <ul>
-                                <li class="filter-check size" data-filter="no"><input class="filter-input" autocomplete="off" id="no-filter-size" type="checkbox" name="Size" checked><label for="no-filter-size">Всі</label></li>
                                 @foreach($sizes as $si)
                                     <li class="filter-check size" data-filter="{{$si->seo_name}}-size"><input class="filter-input" autocomplete="off" id="{{$si->seo_name}}" type="checkbox" name="Size"><label for="{{$si->seo_name}}">{{$si->name}}</label></li>
                                 @endforeach
