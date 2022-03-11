4b22b00048c6007854d4de7f88598b3dcb09802b
aleksandrboichuk <90320430+aleksandrboichuk@users.noreply.github.com>
2022-03-12 01:41:02 +0200

    edits

--- resources/views/parts/banners.blade.php
+++ resources/views/parts/banners.blade.php
@@ -51,15 +51,16 @@
                         @endforeach
 
                     </div>
-                    <a href="#slider-carousel" class="left control-carousel hidden-xs" data-slide="prev">
-                        <i class="fa fa-angle-left"></i>
-                    </a>
-                    <a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next">
-                        <i class="fa fa-angle-right"></i>
-                    </a>
+                    @if(count($banners) > 1)
+                        <a href="#slider-carousel" class="left control-carousel hidden-xs" data-slide="prev">
+                            <i class="fa fa-angle-left"></i>
+                        </a>
+                        <a href="#slider-carousel" class="right control-carousel hidden-xs" data-slide="next">
+                            <i class="fa fa-angle-right"></i>
+                        </a>
+                    @endif
                 </div>
             </div>
-
         </div>
     </div>
 </section>
