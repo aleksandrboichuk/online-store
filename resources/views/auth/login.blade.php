45b168317c923311bb604ab0e4e9bb4d6f04bda9
aleksandrboichuk <90320430+aleksandrboichuk@users.noreply.github.com>
2022-04-16 19:41:04 +0300

    code edits & reset pass by email

--- resources/views/auth/login.blade.php
+++ resources/views/auth/login.blade.php
@@ -25,19 +25,19 @@
         <div class="col-md-4">
 
         </div>
-        <div class="col-md-8 justify-content-center">
+        <div class="col-md-8 auth-form">
             <div class="card">
                 <div class="card-header">{{ __('Вхід') }}</div>
                 <div class="card-body">
                     <form method="POST" action="{{route('login')}}">
                         <div class="row mb-3">
                             {{--<label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email Address') }}</label>--}}
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="email" type="email" placeholder="E-mail" class="form-control"  name="email" value="{{ old('email') }}" required autocomplete="email" autofocus>
                             </div>
 
                            @if($errors->has('error'))
-                                <span class="invalid-feedback" role="alert">
+                                <span class="invalid-feedback auth-feedback" role="alert">
                                      <strong>{{ $errors->first('error') }}</strong>
                                 </span>
                             @endif
@@ -46,13 +46,13 @@
                         <div class="row mb-3">
                             {{--<label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Password') }}</label>--}}
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="password" placeholder="Пароль" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
                             </div>
                         </div>
 
                         <div class="row mb-1">
-                            <div class="col-md-3 offset-md-4">
+                            <div class="col-md-6">
                                 <div class="form-check">
                                     <input class="form-check-input" type="checkbox" name="remember" id="remember" {{ old('remember') ? 'checked' : '' }}>
 
@@ -64,7 +64,7 @@
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-12 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Увійти') }}
                                 </button>
--- resources/views/auth/register.blade.php
+++ resources/views/auth/register.blade.php
@@ -6,7 +6,7 @@
         <div class="col-md-4">
 
         </div>
-        <div class="col-md-8">
+        <div class="col-md-8 auth-form">
             <div class="card">
                 <div class="card-header">{{ __('Реєстрація') }}</div>
 
@@ -17,12 +17,12 @@
                         <div class="row mb-3">
                             {{--<label for="firstname" class="col-md-4 col-form-label text-md-end">{{ __('Ім\'я') }}</label>--}}
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="firstname" type="text" placeholder="Ім'я" class="form-control" name="firstname" value="{{ old('firstname') }}" required autocomplete="firstname" autofocus>
                             </div>
 
                             @error('firstname')
-                            <span class="invalid-feedback" role="alert">
+                            <span class="invalid-feedback auth-feedback"  role="alert">
                                         <strong>{{ $message }}</strong>
                             </span>
                             @enderror
@@ -30,11 +30,11 @@
                         <div class="row mb-3">
                             {{--<label for="lastname" class="col-md-4 col-form-label text-md-end">{{ __('Прізвище') }}</label>--}}
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="lastname" type="text" placeholder="Прізвище" class="form-control" name="lastname" value="{{ old('lastname') }}" required autocomplete="lastname">
                             </div>
                             @error('lastname')
-                            <span class="invalid-feedback" role="alert">
+                            <span class="invalid-feedback auth-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                             </span>
                             @enderror
@@ -42,13 +42,13 @@
                         <div class="row mb-3">
                             {{--<label for="email" class="col-md-4 col-form-label text-md-end">{{ __('Email адреса') }}</label>--}}
 
-                            <div class="col-md-6">
+                            <div class="col-md-12">
 
                                 <input id="email" type="email" placeholder="E-mail" class="form-control" name="email" value="{{ old('email') }}" required autocomplete="email">
 
                             </div>
                             @error('email')
-                            <span class="invalid-feedback" role="alert">
+                            <span class="invalid-feedback auth-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                 </span>
                             @enderror
@@ -56,11 +56,11 @@
 
                         <div class="row mb-3">
                             {{--<label for="password" class="col-md-4 col-form-label text-md-end">{{ __('Пароль') }}</label>--}}
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="password" type="password" placeholder="Пароль" class="form-control" name="password" required autocomplete="new-password">
                             </div>
                             @error('password')
-                            <span class="invalid-feedback" role="alert">
+                            <span class="invalid-feedback auth-feedback" role="alert">
                                         <strong>{{ $message }}</strong>
                                 </span>
                             @enderror
@@ -68,13 +68,13 @@
 
                         <div class="row mb-3">
                             {{--<label for="password-confirm" class="col-md-4 col-form-label text-md-end">{{ __('Підтвердження паролю') }}</label>--}}
-                            <div class="col-md-6">
+                            <div class="col-md-12">
                                 <input id="password-confirm" placeholder="Підтвердження паролю" type="password" class="form-control" name="password_confirmation" required autocomplete="new-password">
                             </div>
                         </div>
 
                         <div class="row mb-0">
-                            <div class="col-md-6 offset-md-4">
+                            <div class="col-md-12 offset-md-4">
                                 <button type="submit" class="btn btn-primary">
                                     {{ __('Зареєструватися') }}
                                 </button>
